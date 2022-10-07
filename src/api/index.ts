43cb5d2ff69841a64d87fa00da82b20b09f9cf3d
Lucas Monteiro <lucas.monteiro58@gmail.com>
2022-10-07 16:39:52 -0300

    config api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,5 @@
 import type { IWeatherResponse } from "@/interfaces/weather";
 
-const API_KEY = "564f17f4c33140ceae2b68e251acc559";
-
 const { VITE_BASE_URL, VITE_RAPID_API_KEY, VITE_RAPID_API_HOST } = import.meta
   .env;
 
@@ -13,30 +11,11 @@ const options = {
   },
 };
 
-export async function getForecast(query: string) {
+export async function getForecast(query: string): Promise<IWeatherResponse> {
   const res = await fetch(
     `https://${VITE_BASE_URL}/forecast.json?q=${query}&days=5`,
     options
   );
   const data = await res.json();
   return data;
-}
-
-export async function getByLatLon(
-  lat: number = -3.757576,
-  lon: number = -38.558531
-): Promise<IWeatherResponse> {
-  const res = await fetch(
-    `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${API_KEY}&include=minutely`
-  );
-  return res.json();
-}
-
-export async function getByCity(
-  city: string = "Fortaleza"
-): Promise<IWeatherResponse> {
-  const res = await fetch(
-    `https://api.weatherbit.io/v2.0/current?city=${city}&key=${API_KEY}&include=minutely`
-  );
-  return res.json();
 }
